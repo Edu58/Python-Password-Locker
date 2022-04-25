d52f43f7de64a63ca425db60f3c8a4547920d199
Edu58 <edumuriithi58@gmail.com>
2022-04-25 15:14:07 +0300

    adds doc strings to the Credentials class

--- credentials.py
+++ credentials.py
@@ -1,22 +1,28 @@
 class Credentials:
+    """This class is used to generate the Credentials object. It reduces repetition"""
 
     credentials_list = []
 
     def __init__(self, website, email, username, password):
+        """Acts like a constructor and is called everytime a new Credential objected is created"""
         self.website = website
         self.email = email
         self.username = username
         self.password = password
 
     def add_credential(self):
+        """Adds the created credential object into the credential_list"""
         Credentials.credentials_list.append(self)
 
     @classmethod
     def display_credentials(cls):
+        """Returns the credential_list with all the credentials stord in it"""
         return cls.credentials_list
 
     @classmethod
     def search_credentials(cls, website):
+        """Receives a website parameter and searches for a credential in the credential_list belonging to the website
+        and returns the whole credential """
         for credential in cls.credentials_list:
             if credential.website == website:
                 return credential
@@ -25,6 +31,8 @@ class Credentials:
 
     @classmethod
     def delete_credential(cls, website):
+        """Receives a website parameter and searches for a credential in the credential_list belonging to the website
+                and deletes the whole credential"""
         for credential in cls.credentials_list:
             if credential.website == website:
                 cls.credentials_list.remove(credential)
