cebd1f39037749043a050f0c326674f59b3493e3
Edu58 <edumuriithi58@gmail.com>
2022-04-25 12:46:39 +0300

    adds add_credential method and credentials_list

--- credentials.py
+++ credentials.py
@@ -1,7 +1,13 @@
 class Credentials:
 
+    credentials_list = []
+
     def __init__(self, website, email, username, password):
         self.website = website
         self.email = email
         self.username = username
         self.password = password
+
+    def add_credential(self):
+        Credentials.credentials_list.append(self)
+
