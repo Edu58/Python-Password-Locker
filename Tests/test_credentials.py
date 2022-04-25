f31ac9c45f28c7caf792c300f2f1b1bd269834ff
Edu58 <edumuriithi58@gmail.com>
2022-04-25 15:07:18 +0300

    adds doc strings to the test files

--- Tests/test_credentials.py
+++ Tests/test_credentials.py
@@ -3,25 +3,31 @@ from credentials import Credentials
 
 
 class MyTestCase(unittest.TestCase):
+    """This class tests the credentials.py functions"""
 
     def setUp(self) -> None:
+        """Creates a new test credential for every test case to use"""
         self.new_credentials = Credentials('facebook', 'ed@gmail.com', 'ed', 'edd12345')
 
     def tearDown(self) -> None:
+        """Clears up the credentials_list after every test case"""
         Credentials.credentials_list = []
 
-    def test_credendtial_created(self):
+    def test_credential_created(self):
+        """Tests if the credential object is created successfully"""
         self.assertEqual(self.new_credentials.website, 'facebook')
         self.assertEqual(self.new_credentials.email, 'ed@gmail.com')
         self.assertEqual(self.new_credentials.username, 'ed')
         self.assertEqual(self.new_credentials.password, 'edd12345')
 
     def test_add_credential(self):
+        """Tests if the credential object is added to the credential successfully"""
         self.new_credentials.add_credential()
 
         self.assertEqual(len(Credentials.credentials_list), 1)
 
     def test_display_credentials(self):
+        """Tests if all the credentials are returned successfully"""
         self.new_credentials.add_credential()
 
         test_credential = Credentials('instagram', 'inst@gmail.com', 'vapor', 'insta4234')
@@ -30,6 +36,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(Credentials.display_credentials(), Credentials.credentials_list)
 
     def test_delete_credential(self):
+        """Tests if the credential belonging to the provided website is deleted successfully"""
         self.new_credentials.add_credential()
 
         test_credential = Credentials('tiktok', 'test@gmail.com', 'tiktoker', 'tiktoker9876')
@@ -39,6 +46,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(len(Credentials.credentials_list), 1)
 
     def test_search_credentials(self):
+        """Tests if the credential for the searched website is returned successfully"""
         self.new_credentials.add_credential()
 
         test_search = Credentials.search_credentials('facebook')
@@ -46,6 +54,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(test_search, self.new_credentials)
 
     def test_search_credentials_not_found(self):
+        """Tests if an error is returned if a credential belonging to the provided website is not found"""
         self.new_credentials.add_credential()
 
         test_search = Credentials.search_credentials('instagram')
--- Tests/test_user.py
+++ Tests/test_user.py
@@ -3,27 +3,28 @@ from user import User
 
 
 class MyTestCase(unittest.TestCase):
+    """This class tests the user.py functions"""
 
-    """Creates a new test user for every test case to use"""
     def setUp(self):
+        """Creates a new test user for every test case to use"""
         self.new_user = User('edwin', 'eddy123')
 
-    """Clears up the user_list after every test case"""
     def tearDown(self) -> None:
+        """Clears up the user_list after every test case"""
         User.users_list = []
 
-    """Tests if the user object is created successfully"""
     def test_if_user_is_created_correctly(self):
+        """Tests if the user object is created successfully"""
         self.assertEqual(self.new_user.username, 'edwin')
         self.assertEqual(self.new_user.password, 'eddy123')
 
-    """Tests if the created user is successfully added to the user_list"""
     def test_add_user(self):
+        """Tests if the created user is successfully added to the user_list"""
         self.new_user.add_user()
         self.assertEqual(len(self.new_user.users_list), 1)
 
-    """Tests if the a user is successfully deleted from the user_list"""
     def test_delete_user(self):
+        """Tests if the a user is successfully deleted from the user_list"""
         self.new_user.add_user()
 
         test_user = User('ed', 'ed456')
@@ -32,15 +33,15 @@ class MyTestCase(unittest.TestCase):
         self.new_user.delete_user()
         self.assertEqual(len(User.users_list), 1)
 
-    """Tests if the user is logged in successfully on entering correct details"""
     def test_successful_login(self):
+        """Tests if the user is logged in successfully on entering correct details"""
         self.new_user.add_user()
 
         correct_login = User.login('edwin', 'eddy123')
         self.assertTrue(correct_login)
 
-    """Tests if the user is blocked from logging in if the details provided are wrong"""
     def test_failed_login(self):
+        """Tests if the user is blocked from logging in if the details provided are wrong"""
         self.new_user.add_user()
 
         wrong_login = User.login('edwin', 'eddy12')
