b06d124d661f7e4cdf9f72da3b794ef5f165c751
Edu58 <edumuriithi58@gmail.com>
2022-04-25 11:49:04 +0300

    adds User class methods

--- user.py
+++ user.py
@@ -1,6 +1,22 @@
 class User:
 
+    users_list = []
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def add_user(self, username, password):
+        User.users_list.append(self)
+
+    def delete_user(self):
+        User.users_list.remove(self)
+
+    @classmethod
+    def login(cls, username, password):
+        for user in cls.users_list:
+            if user.username == username and user.password == password:
+                return True
+            else:
+                return False
 
