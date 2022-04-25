963a5aabcba8803d1ebf8e33ef6867ef4685ef1c
Edu58 <edumuriithi58@gmail.com>
2022-04-25 11:57:18 +0300

    adds test for user initialization

--- Tests/test_user.py
+++ Tests/test_user.py
@@ -1,9 +1,18 @@
 import unittest
+from user import User
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.new_user = User('edwin', 'eddy123')
+
+    def tearDown(self) -> None:
+        self.new_user.users_list = []
+
+    def test_if_user_is_created_correctly(self):
+        self.assertEqual(self.new_user.username, 'edwin')
+        self.assertEqual(self.new_user.password, 'eddy123')
 
 
 if __name__ == '__main__':
