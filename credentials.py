7a1f35265ad50e170675488aca4290d42e579482
Edu58 <edumuriithi58@gmail.com>
2022-04-25 12:31:28 +0300

    adds credentials.py constructor

---
+++ credentials.py
@@ -0,0 +1,7 @@
+class Credentials:
+
+    def __init__(self, website, email, username, password):
+        self.website = website
+        self.email = email
+        self.username = username
+        self.password = password
