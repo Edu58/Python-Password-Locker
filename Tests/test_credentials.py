7fb77dc96661271c580483408dc6526cc7bb9c60
Edu58 <edumuriithi58@gmail.com>
2022-04-25 12:42:29 +0300

    adds test for credential created and add_credential

--- Tests/test_credentials.py
+++ Tests/test_credentials.py
@@ -8,7 +8,18 @@ class MyTestCase(unittest.TestCase):
         self.new_credentials = Credentials('facebook', 'ed@gmail.com', 'ed', 'edd12345')
 
     def tearDown(self) -> None:
-        Credentials.credential_list = []
+        Credentials.credentials_list = []
+
+    def test_credendtial_created(self):
+        self.assertEqual(self.new_credentials.website, 'facebook')
+        self.assertEqual(self.new_credentials.email, 'ed@gmail.com')
+        self.assertEqual(self.new_credentials.username, 'ed')
+        self.assertEqual(self.new_credentials.password, 'edd12345')
+
+    def test_add_credential(self):
+        self.new_credentials.add_credential()
+
+        self.assertEqual(len(Credentials.credentials_list), 1)
 
 
 if __name__ == '__main__':
